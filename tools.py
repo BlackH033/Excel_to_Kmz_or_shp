d9ea9a814b29c6f1db1336e130c480c55ceac9b0
Victor Rios <95384317+BlackH033@users.noreply.github.com>
2023-08-04 17:39:33 -0500

    Add files via upload

--- tools.py
+++ tools.py
@@ -1,74 +1,280 @@
+#--------------librerias --------------
 import fiona
 import geopandas as gpd
+import os
+import shutil
+#--------------------------------------
+
+#-------------- Clase generador() --------------
+# continene los metodos necesarios para el procesamiento 
 
 class generador():
+    """
+    La clase contiene los metodos encargados del procesamiento de la 
+    información suministrada en el archivo Excel.
+    """
     def __init__(self):
         super().__init__()
     
+    # ---------- Metodo para generar Shapefile de tipo puntos -----------
     def puntos(self,data,ruta,nombre,sis_coor):
-        esquema={'geometry':'Point','properties':[('Name','str')]}
-        shape=fiona.open(ruta+"/"+nombre+".shp", mode='w', driver='ESRI Shapefile',schema = esquema, crs = sis_coor)
-        for _, row in data.iterrows():
-            punto_propiedad={
-                'geometry' : {'type':'Point',
-                            'coordinates': (float(row.x),float(row.y))},
-                'properties': {'Name' : row.id},
+        """
+        recibe 4 parametros: 
+            1. data     -> DataFrame con la información de las coordenadas
+                           el DataFrame lo crea pandas cuando se abre el archivo Excel
+            2. ruta     -> La ruta donde se guardaran los archivos
+            3. nombre   -> nombre que el usuario desea que lleve los archivos
+            4. sis_coor -> el sistema de coordenadas en el cual están las coordenadas
+
+        genera el shape de tipo punto (genera 5 archivos .shp,.cpg,.dbf,.prj,.shx) en la ruta especificada.
+        """
+
+        """
+        esquema que llevará el shape, geometria de tipo punto y de propiedades un campo str llamado "id"
+        """
+        esquema={
+            "geometry":"Point",
+            "properties":[("id","str")]                
             }
-            shape.write(punto_propiedad)
+        """
+        fiona.open abre archivos, en este caso .shp
+        fiona.open detecta que el archivo no existe y lo crea.
+        se crea el shape vacío teniendo encuenta: 
+            1. la ruta donde se guardará el archivo y el nombre dado por el usuario. 
+               ruta+"/"+nombre+".shp" -> en la ruta se crea el achivo "nombre".shp
+            2. se le idica que el archivo se abre en modo escritura  mode="w"
+            3. se indica el diver, driver="ESRI Shapefile" indica que es formato shapefile
+            4. el esquema creado en la variable anterior. indicando la estructura del shapefile
+            5. el sistema de coordenadas en el cual se trabajará. 
+               crs = sis_coor 
+        """                          
+        shape=fiona.open(
+            ruta+"/"+nombre+".shp", 
+            mode='w', driver="ESRI Shapefile",
+            schema = esquema, 
+            crs = sis_coor)
+        """
+        se usa un for para recorrer la información que genera data.iterrows().
+        data.iterrows() -> devuelve un iterador del DataFrame "data" con dos objetos: un objeto con 
+                           indices y un objeto con la informacion.
+        "_" toma como valor cada objeto indice (esta información no se usa)
+        "row" toma el valor del objeto con la información
+
+        por cada iteración del for se creará un punto en el shape.
+        para ello se crea el "esquema" que llevará cada punto, donde se indica la geometria la cual lleva:
+            - el tipo de geometria ("type":"Point")
+            - las coordenadas indicando coordenada X y coordenada Y ("coordinates": (row.x,row.y))
+                como row es el objeto que almacena la información se extrae la información de "x" y "y" 
+        además se agrega en el esquema la propiedad "id" y su valor ("properties:{"id":row.id})
+        
+        se usa .write para agregar el esquema al shape (shape.write(punto_esquema)  -> así en el shape se guarda cada punto)
+        al finalizar el for se cierra el shape y ya queda el shape creado con todos los puntos que trae el DataFrame
+        """
+        for _, row in data.iterrows():                                       
+            punto_esquema={                                                
+                "geometry" : {"type":"Point",                               
+                            "coordinates": (row.x,row.y)},                   
+                "properties": {"id" : row.id},
+            }
+            shape.write(punto_esquema)
         shape.close()
     
+    # ---------- Metodo para generar Shapefile de tipo linea -----------
     def lineas(self,data,ruta,nombre,sis_coor):
-        esquema={'geometry':'LineString','properties':[('Name','str')]}
-        shape=fiona.open(ruta+"/"+nombre+".shp", mode='w', driver='ESRI Shapefile',schema = esquema, crs = sis_coor)
+        """
+        recibe 4 parametros: 
+            1. data     -> DataFrame con la información de las coordenadas
+                           el DataFrame lo crea pandas cuando se abre el archivo Excel
+            2. ruta     -> La ruta donde se guardaran los archivos
+            3. nombre   -> nombre que el usuario desea que lleve los archivos
+            4. sis_coor -> el sistema de coordenadas en el cual están las coordenadas
+
+        genera el shape de tipo linea (genera 5 archivos .shp,.cpg,.dbf,.prj,.shx) en la ruta especificada.
+        """
+
+        """
+        esquema que llevará el shape, geometria de tipo linea y de propiedades un campo str llamado "id"
+        """
+        esquema={
+            "geometry":"LineString",
+            "properties":[("id","str")]
+            }
+        """
+        fiona.open abre archivos, en este caso .shp
+        fiona.open detecta que el archivo no existe y lo crea.
+        se crea el shape vacío teniendo en cuenta: 
+            1. la ruta donde se guardará el archivo y el nombre dado por el usuario. 
+               ruta+"/"+nombre+".shp" -> en la ruta se crea el achivo "nombre".shp
+            2. se le idica que el archivo se abre en modo escritura  mode="w"
+            3. se indica el diver, driver="ESRI Shapefile" indica que es formato shapefile
+            4. el esquema creado en la variable anterior. indicando la estructura del shapefile
+            5. el sistema de coordenadas en el cual se trabajará. 
+               crs = sis_coor 
+        """ 
+        shape=fiona.open(
+            ruta+"/"+nombre+".shp", 
+            mode="w", driver="ESRI Shapefile",
+            schema = esquema, 
+            crs = sis_coor
+            )
+        """
+        se creea una lista vacía donde se almacenarán las coordenadas de cada punto conexión de linea.
+        se usa un for para recorrer la información que genera data.iterrows().
+        data.iterrows() -> devuelve un iterador del DataFrame "data" con dos objetos: un objeto con 
+                           indices y un objeto con la informacion.
+        "_" toma como valor cada objeto indice (esta información no se usa)
+        "row" toma el valor del objeto con la información
+
+        por cada iteración se guarda en la lista una tupla que contiene la coordenada "x" y "y" 
+        de cada punto (lista_puntos.append((row.x,row.y)))
+        como row es el objeto que almacena la información se extrae la información de "x" y "y"
+        """
         lista_puntos=[]
         for _, row in data.iterrows():
             lista_puntos.append((row.x,row.y))
-        sh_propiedad = {
-        'geometry' : {'type':'LineString',
-                        'coordinates': lista_puntos},
-        'properties': {'Name' : nombre}, 
+        """
+        se crea el esquema de la linea indicando la geometria, la cual tiene:
+            - el tipo de geomtria, que en este caso es linea ("type":"LineString")
+            - las coordenadas, que están almacenadas en la lista "lista_puntos" ("coordinates": lista_puntos)
+        además se agrega en el esquema la propiedad "id" y su valor ("properties:{"id":nombre})
+
+        se usa .write para agregar el esquema al shape (shape.write(linea_esquema)
+        se cierra el shape y ya queda el shape de linea creado con todos los puntos que trae el DataFrame
+        """
+        linea_esquema = {
+        "geometry" : {"type":"LineString",
+                        "coordinates": lista_puntos},
+        "properties": {"id" : nombre}, 
         }                                           
-        shape.write(sh_propiedad)
+        shape.write(linea_esquema)
         shape.close()
 
+    # ---------- Metodo para generar Shapefile de tipo poligono -----------
     def poligono(self,data,ruta,nombre,sis_coor):
+        """
+        recibe 4 parametros: 
+            1. data     -> DataFrame con la información de las coordenadas
+                           el DataFrame lo crea pandas cuando se abre el archivo Excel
+            2. ruta     -> La ruta donde se guardaran los archivos
+            3. nombre   -> nombre que el usuario desea que lleve los archivos
+            4. sis_coor -> el sistema de coordenadas en el cual están las coordenadas
+
+        genera el shape de tipo poligono (genera 5 archivos .shp,.cpg,.dbf,.prj,.shx) en la ruta especificada.
+        """
+
+        """
+        esquema que llevará el shape, geometria de tipo poligono y de propiedades un campo str llamado "id"
+        """
         esquema = {
-        'geometry':'Polygon',
-        'properties':[('Name','str')]
+        "geometry":"Polygon",
+        "properties":[("id","str")]
         }
-        shape=fiona.open(ruta+"/"+nombre+".shp", mode='w', driver='ESRI Shapefile',schema = esquema, crs = sis_coor)
-        lista_puntos = []
+        """
+        fiona.open abre archivos, en este caso .shp
+        fiona.open detecta que el archivo no existe y lo crea.
+        se crea el shape vacío teniendo en cuenta: 
+            1. la ruta donde se guardará el archivo y el nombre dado por el usuario. 
+               ruta+"/"+nombre+".shp" -> en la ruta se crea el achivo "nombre".shp
+            2. se le idica que el archivo se abre en modo escritura  mode="w"
+            3. se indica el diver, driver="ESRI Shapefile" indica que es formato shapefile
+            4. el esquema creado en la variable anterior. indicando la estructura del shapefile
+            5. el sistema de coordenadas en el cual se trabajará. 
+               crs = sis_coor 
+        """ 
+        shape=fiona.open(
+            ruta+"/"+nombre+".shp", 
+            mode="w", 
+            driver="ESRI Shapefile",
+            schema = esquema, 
+            crs = sis_coor)
+        """
+        se creea una lista vacía donde se almacenarán las coordenadas de cada vertice que conforma el poligono.
+        se usa un for para recorrer la información que genera data.iterrows().
+        data.iterrows() -> devuelve un iterador del DataFrame "data" con dos objetos: un objeto con 
+                           indices y un objeto con la informacion.
+        "_" toma como valor cada objeto indice (esta información no se usa)
+        "row" toma el valor del objeto con la información
+
+        por cada iteración se guarda en la lista una tupla que contiene la coordenada "x" y "y" 
+        de cada vertice (lista_vertice.append((row.x,row.y)))
+        como row es el objeto que almacena la información se extrae la información de "x" y "y"
+        """
+        lista_vertice = []
         for _, row in data.iterrows():
-            lista_puntos.append((float(row.x),float(row.y)))
-        lista_puntos.append(lista_puntos[0])
+            lista_vertice.append((float(row.x),float(row.y)))
+        lista_vertice.append(lista_vertice[0])
         shp_propiedad = {
-        'geometry' : {'type':'Polygon',
-                        'coordinates': [lista_puntos]},
-        'properties': {'Name' : nombre},
+        "geometry" : {"type":"Polygon",
+                        "coordinates": [lista_vertice]},
+        "properties": {"id" : nombre},
         }                                       
         shape.write(shp_propiedad)
         shape.close()
     
+    # ---------- Metodo para generar .kml -----------
     def crear_KML(self,ruta,tipo,nombre):
-        if tipo==0:
-            shp=gpd.read_file(ruta+"/"+nombre+".shp")
-            gpd.io.file.fiona.drvsupport.supported_drivers['KML']='rw'
-            ruta=ruta.split("/")
-            if len(ruta)>1:ruta.pop(-1)
-            ruta="/".join(ruta)
-            shp.to_file(ruta+"/"+nombre+".kml",driver="KML")
-        else:
-            shp=gpd.read_file(ruta+"/"+nombre+".shp")
-            gpd.io.file.fiona.drvsupport.supported_drivers['KML']='rw'
-            shp.to_file(ruta+"/"+nombre+".kml",driver="KML")
+        """
+        Este metodo recibe 3 parametros: 
+            1. ruta -> la ruta en la cual se encuentran almacenados los .shp
+            2. tipo -> este parametro puede ser 0 o 1.
+                        0 quiere decir que el ususario elgió solo crear el .kml
+                        1 quiere decir que el usuario eligió crear .kml y .shp
+            3. nombre -> el nombre que el usuario elgió para nombrar el archivo
 
-    def crear_linea_puntos(self,ruta,ruta2,nombre):
-        kml1=open(ruta+"/linea.kml","r").readlines() 
-        kml2=open(ruta+"/puntos.kml","r").readlines() 
-        nuevo=open(ruta2+"/"+nombre+".kml","w") 
-        for i in kml1[:-1]: 
-            nuevo.write(i) 
-        for i in kml2[3:]: 
-            nuevo.write(i) 
-        nuevo.close()    
+        - el tipo 0 trabaja desde una carpeta momentanea que se crea en main.py,
+        - el tipo 1 trabaja sobre la misma carpeta donde se guardan los .shp
+        """
+        
+        if tipo==0:            
+            shp=gpd.read_file(ruta+"/"+nombre+".shp")                   #abre el archivo .shp creado en la carpeta que se encuentra en la ruta (que es una carpeta momentanea)    
+            ruta=ruta.split("/")                                        #la ruta la toma como lista según los niveles lvl1/lvl2/lvl3 -> [lvl1,lvl2,lvl3]
+            ruta.pop(-1)                                                # elimina un nivel, ya que no se quiere guardar en esa carpeta (esa carpeta es momentanea y se borrará)
+            ruta="/".join(ruta)                                         # convierte a str la lista que contiene la ruta
+            gpd.io.file.fiona.drvsupport.supported_drivers['KML']='rw'  #habilita el driver para leer archivos .kml
+            shp.to_file(ruta+"/"+nombre+".kml",driver="KML")            # convierte el .shp en .kml con .to_file indicando el driver="KML" 
+        
+        elif tipo==1:
+            shp=gpd.read_file(ruta+"/"+nombre+".shp")                   #abre el archivo .shp creado en la carpeta que se encuentra en la ruta (/shape)
+            gpd.io.file.fiona.drvsupport.supported_drivers['KML']='rw'  #habilita el driver para leer archivos .kml
+            shp.to_file(ruta+"/"+nombre+".kml",driver="KML")            #convierte el .shp a .kml y lo guarda en la misma carpeta
 
+    # ---------- Metodo para generar .kml con linea y puntos a la vez -----------
+    def crear_linea_puntos(self,data,ruta,nombre,sis_coor):
+        """
+        Este metodo recibe 4 parametros:
+            1. data     -> DataFrame con la información de las coordenadas
+                           el DataFrame lo crea pandas cuando se abre el archivo Excel
+            2. ruta     -> la ruta donde se trabajará y guardaran archivos
+            3. nombre   -> el nombre con el que el usuario quiere guardar el archivo
+            4. sis_coor -> sistema de cooordenadas en el que están la información del DataFrame
+
+        - se crea una carpeta momentanea pa almacenar archivos necesarios y luego la borra (la carpeta se llamará "00000000carpetamomentanea")
+        para este metodo se parte de que un .kml es un archivo basado en XML. se usarán dos kml y se unirán.
+
+        los .kml generados por la app siempre inician con 3 lineas
+                <?xml version="1.0" encoding="utf-8" ?>
+                <kml xmlns="http://www.opengis.net/kml/2.2">
+                <Document id="root_doc">
+        de resto son lineas con la información pertinente para la geometria.
+        y cierran con la misma linea:
+                </Document></kml>
+
+        teniendo encuenta esto se toma el primer .kml sin tener encuenta la ultima linea (no se tiene encuenta la linea en posición -1 -> que es el cierre del archivo)
+        seguido de eso se toma el segundo .kml y se toma apartir de la linea 4. esto con el fin de que no tome las 3 primeras linea mencionadas anteriormente.
+        estas dos partes se unen en un solo documento .kml
+        """
+        self.generador=generador()
+        os.makedirs(ruta+"/00000000carpetamomentanea")                                       #carpeta momentanea
+        self.generador.puntos(data,ruta+"/00000000carpetamomentanea","puntos",sis_coor)      #crea un .shp de tipo punto con el nombre "puntos" en la carpeta momentanea
+        self.generador.lineas(data,ruta+"/00000000carpetamomentanea","linea",sis_coor)       #crea un .shp de tipo linea con el nombre "linea" en la carpeta momentanea
+        self.generador.crear_KML(ruta+"/00000000carpetamomentanea",1,"puntos")               #crea un .kml con el nombre "puntos" usando el .shp creado y que se encuentra en la carpeta momentanea
+        self.generador.crear_KML(ruta+"/00000000carpetamomentanea",1,"linea")                #crea un .kml con el nombre "linea" usando el .shp creado y que se encuentra en la carpeta momentanea
+        kml1=open(ruta+"/00000000carpetamomentanea"+"/linea.kml","r").readlines()            #abre el .kml creado con el nombre "linea" y se almacena en una lista
+        kml2=open(ruta+"/00000000carpetamomentanea"+"/puntos.kml","r").readlines()           #abre el .kml creado con el nombre "puntos" y se almacena en una lista
+        nuevo=open(ruta+"/"+nombre+".kml","w")                                               #se crea un nuevo archivo .kml con el nombre dado por el usuario
+        for i in kml1[:-1]:                                                                  #se toma el primer .kml sin tener en cuenta la ultima linea
+            nuevo.write(i)                                                                   #se agrega linea por linea al nuevo kml
+        for i in kml2[3:]:                                                                   #se toma el segundo .kml sin tener encuenta las 3 primeras lineas
+            nuevo.write(i)                                                                   #se agrega linea por linea al nuevo kml
+        nuevo.close()                                                                        #se cierra el nuevo kml
+        shutil.rmtree(ruta+"/00000000carpetamomentanea")                                     #se elimina la carpeta momentanea       
--- windows.py
+++ windows.py
@@ -1,34 +1,64 @@
+#--------------librerias --------------
 import customtkinter
 import os
 from PIL import Image
+#--------------------------------------
+
+#-------------- Clase ventana_secundaria --------------
 
 class ventana_secundaria(customtkinter.CTkToplevel):
-    carpeta_raiz=os.path.dirname(__file__)
-    carpeta_img=os.path.join(carpeta_raiz,"img")
+    """
+    Cuando se llama la clase, customtkinter.CTKToplevel genera una ventana secundaria vacía.
+    los metodos contenidos en la clase se usan para llenar la ventana secundaria con la 
+    información necesaria (icono, mensaje y boton)
+    """
+
+    carpeta_raiz=os.path.dirname(__file__)                 #guarda la ruta donde se encuentra este archivo .py
+    carpeta_img=os.path.join(carpeta_raiz,"img")           #crea la ruta relativa a la carpeta /img - la cual se guarda en la misma ruta del archivo .py
     
     def __init__(self):
         super().__init__()
         self.grab_set()
-        self.resizable(width=False, height=False)
+        self.resizable(width=False, height=False)          #no permite cambiar el tamaño de la ventana
   
     def boton_cerrar_rojo(self,index=int):
+        """
+        boton_cerrar_rojo(index) -> crea un boton con el texto "CERRAR" en color rojo   
+                                    recibe como parametro la posicion fila donde se ubicará
+        """
         self.btn_cerrar = customtkinter.CTkButton(self, text ="CERRAR", command = self.destroy,fg_color="red",hover_color="#A50000")
         self.btn_cerrar.grid(row=index,column=0,sticky="ew",padx=60,pady=(20, 30))
     
     def boton_cerrar_verde(self,index=int):
+        """
+        boton_cerrar_verde(index) -> crea un boton con el texto "CERRAR" en color verde  
+                                    recibe como parametro la posicion fila donde se ubicará
+        """
         self.btn_cerrar = customtkinter.CTkButton(self, text ="CERRAR", command = self.destroy,fg_color="green",hover_color="#0DAF0A")
         self.btn_cerrar.grid(row=index,column=0,sticky="ew",padx=60,pady=(20, 30))
     
     def boton_cerrar_amarillo(self,index=int):
+        """
+        crea un boton con el texto "CERRAR" en color amarillo  
+        recibe como parametro la posicion fila donde se ubicará
+        """
         self.btn_cerrar = customtkinter.CTkButton(self, text ="CERRAR", command = self.destroy,fg_color="#D68910",hover_color="#7E5109")
         self.btn_cerrar.grid(row=index,column=0,sticky="ew",padx=60,pady=(20, 30))
     
     def icono(self,name=str):
+        """
+        crea el icono en la ventana de aviso 
+        recibe como parametro el nombre del icono a mostrar
+        """
         self.iconoerror=customtkinter.CTkImage(Image.open(os.path.join(ventana_secundaria.carpeta_img,name+".png")),size=(100,100))
         self.iconoerror=customtkinter.CTkLabel(self, image = self.iconoerror,text="")
         self.iconoerror.grid(row=0,column=0,pady=(20,20),sticky="nsew")
             
     def error_carpeta_formato(self):
+        """
+        rellena la ventana secundaria con la información necesaria 
+        para indicar que hubo error generando el formato .xlsx
+        """
         self.title("!!ERROR!!")
         self.icono("cancelar")
         self.texterror=customtkinter.CTkLabel(self,text="Error generando el formato",font=customtkinter.CTkFont(size=18,weight="bold"))
@@ -36,6 +66,11 @@ class ventana_secundaria(customtkinter.CTkToplevel):
         self.boton_cerrar_rojo(2)
     
     def correcto_formato(self,ruta=str):
+        """
+        rellena la ventana secundaria con la información necesaria para 
+        indicar que hubo error generando el formato .xlsx .
+        recibe como parametro la ruta donde se generó el formato
+        """
         self.title("Correcto")
         self.icono("comprobado")
         self.textcorrecto=customtkinter.CTkLabel(self,text="Formato generado correctamente en",font=customtkinter.CTkFont(size=18,weight="bold"))
@@ -46,6 +81,11 @@ class ventana_secundaria(customtkinter.CTkToplevel):
         self.boton_cerrar_verde(3)
     
     def precaucion_formato(self,ruta=str):
+        """
+        rellena la ventana secundaria con la información necesaria para 
+        indicar que ya existe el formato .xlsx .
+        recibe como parametro la ruta donde se encuentra el formato
+        """
         self.title("Precaucion")
         self.icono("precaucion")
         self.textp=customtkinter.CTkLabel(self,text="Formato ya existe en la carpeta",font=customtkinter.CTkFont(size=18,weight="bold"))
@@ -54,15 +94,13 @@ class ventana_secundaria(customtkinter.CTkToplevel):
         self.textbox.insert("0.0",ruta)
         self.textbox.grid(row=2, column=0, sticky="nsew",padx=20)
         self.boton_cerrar_amarillo(3)
-   
-    def error_seleccion(self):
-        self.title("!!ERROR!!")
-        self.icono("cancelar")
-        self.texterror=customtkinter.CTkLabel(self,text="Seleccione un archivo",font=customtkinter.CTkFont(size=18,weight="bold"))
-        self.texterror.grid(row=1,column=0,padx=40,sticky="nsew")
-        self.boton_cerrar_rojo(2)
     
     def creado_correcto(self,ruta):
+        """
+        rellena la ventana secundaria con la información necesaria para 
+        indicar que se genraron los archivos correctamente .
+        recibe como parametro la ruta donde se guardaron los archivos
+        """
         self.title("Correcto")
         self.icono("comprobado")
         self.textcorrecto=customtkinter.CTkLabel(self,text="Creado correctamente en",font=customtkinter.CTkFont(size=18,weight="bold"))
@@ -73,6 +111,10 @@ class ventana_secundaria(customtkinter.CTkToplevel):
         self.boton_cerrar_verde(3)
     
     def error_crear(self):
+        """
+        rellena la ventana secundaria con la información necesaria para 
+        indicar que no el usuario no agregó el archivo Excel .
+        """
         self.title("!!ERROR!!")
         self.icono("cancelar")
         self.texterror=customtkinter.CTkLabel(self,text="Debe cargar el archivo Excel",font=customtkinter.CTkFont(size=18,weight="bold"))
@@ -80,6 +122,12 @@ class ventana_secundaria(customtkinter.CTkToplevel):
         self.boton_cerrar_rojo(2)
 
     def archivo_existe(self,ruta,nombre):
+        """
+        rellena la ventana secundaria con la información necesaria para 
+        indicar que se los archivos que se quieren generar ya existen con ese nombre .
+        recibe como parametro la ruta donde se encuentran los archivos 
+        y el nombre de los archivos
+        """
         self.title("!!ERROR!!")
         self.icono("precaucion")
         self.textp=customtkinter.CTkLabel(self,text="Archivo con el nombre '"+nombre+"' ya existe en la carpeta",font=customtkinter.CTkFont(size=18,weight="bold"))
